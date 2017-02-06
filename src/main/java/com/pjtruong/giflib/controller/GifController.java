c7515919301da78f3e683a121765a4b6933781ee
Peter J. Truong <hp5340@gmail.com>
2017-02-06 11:40:50 -0800

    Web app for a library of gifs.

---
+++ src/main/java/com/pjtruong/giflib/controller/GifController.java
@@ -0,0 +1,45 @@
+package com.pjtruong.giflib.controller;
+
+import com.pjtruong.giflib.data.GifRepository;
+import com.pjtruong.giflib.model.Gif;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.time.LocalDate;
+import java.util.List;
+
+/**
+ * Created by pjtruong on 2/6/17.
+ */
+
+@Controller
+public class GifController {
+
+    @Autowired
+    private GifRepository gifRepository;
+
+
+    @RequestMapping("/")
+    public String listGifs(ModelMap modelMap)
+    {
+       List<Gif> allGifs = gifRepository.getAllGifs();
+        modelMap.put("gifs", allGifs);
+
+        return "home";
+    }
+
+    @RequestMapping("/gif/{name}")
+    public String gifDetails(@PathVariable String name, ModelMap modelMap)
+    {
+
+        Gif gif = gifRepository.findByName(name);
+        modelMap.put("gif", gif);
+        return "gif-details";
+    }
+
+
+}
